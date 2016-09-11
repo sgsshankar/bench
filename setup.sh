4cbc10da3dc171cfbb902d6f4ad81e3cd44716a4
sgsshankar <sgsshankarnarayanan@gmail.com>
2016-09-11 23:22:55 +0530

    Removed OS Dependency and added Python requirement

--- setup.sh
+++ setup.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if [ -f /etc/redhat-release ] ; then
-    sudo apt-get install libmysqlclient-dev python-dev
-elif [ -f /etc/lsb-release ] ; then
-    sudo apt-get install libmysqlclient-dev python-dev
-fi
-
 sudo pip -r requirements.txt
